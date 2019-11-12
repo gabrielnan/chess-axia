7e842beb5441aaed2b8f3f4aea0ff05e1f13bfbb
Travis Burrows <37048572+tjburrows@users.noreply.github.com>
2019-11-12 09:51:24 -0500

    added legal moves and chessjs integration

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -3,18 +3,23 @@ $( document ).ready(function() {
 	//	Board name that matches div
     var boardName = 'myBoard'
 	
+	//	Initialize chess js object
+	var game = new Chess()
+	
+	//	Start chess as cleared
+	game.clear()
+	
     //  Hard code start configuration
 	var positions = {}
-	positions['e4'] = 'bK'
-	positions['f4'] = 'wP'
     
     //  Initialize board configuration
 	var boardConfig = {
 		pieceTheme: '/static/{piece}.png',	//	location of piece images
 		position:positions,					//	initial piece positions dictionary
 		draggable: true,					//	pieces are draggable
-		dropOffBoard: 'trash',				//	What happens when piece is dragged off board
 		onSnapEnd: onSnapEnd,				//	Function to call after piece is snapped to a new position
+		onDrop:onDrop,						//	Function to call piece is dropped (before snap)
+		onChange:onChange,					//	Function to call when board position changes
 		sparePieces: true					//	Draws spare black and white pieces on either side of the board
 	}
     
@@ -22,6 +27,7 @@ $( document ).ready(function() {
 	var svgW = 400
 	var svgH = 100
 	var svgTextSize = 15
+	var svgVerticalSpace = svgTextSize+3
 	var svg = d3.select('#boardConfig')
 		.append('svg')
 		.attr("width", svgW)
@@ -38,10 +44,26 @@ $( document ).ready(function() {
 	//	Text object for listing values
     var valueText = svg.append('text')
 		.attr('x',0)
-		.attr('y', svgTextSize)
+		.attr('y', svgVerticalSpace)
 		.attr('dominant-baseline', 'hanging')
 		.attr('text-anchor', 'start')  
 		.style('font-size', svgTextSize + 'px')
+		
+	var nextTurnText = svg.append('text')
+		.attr('x',0)
+		.attr('y', 2*svgVerticalSpace)
+		.attr('dominant-baseline', 'hanging')
+		.attr('text-anchor', 'start')  
+		.style('font-size', svgTextSize + 'px')
+		.text('Next Turn: ')
+		
+	var messageText = svg.append('text')
+		.attr('x',0)
+		.attr('y', 3*svgVerticalSpace)
+		.attr('dominant-baseline', 'hanging')
+		.attr('text-anchor', 'start')  
+		.style('font-size', svgTextSize + 'px')
+		.text('Message: ')
     
     //  Create chess board
 	var board = Chessboard(boardName,boardConfig)
@@ -62,7 +84,6 @@ $( document ).ready(function() {
         .attr('width', colorBarWidth/numBars)
         .attr('height',20)
         .style('fill',color(b/numBars))
-        
     }
 	
 	//	Clears custom color under a single square
@@ -72,6 +93,7 @@ $( document ).ready(function() {
 
     //  Function ran when piece moves
 	function onSnapEnd(source=null, target=null, piece=null){	
+		
 		//	Get current board positions
 		var boardPositions = board.position()
 		
@@ -84,8 +106,11 @@ $( document ).ready(function() {
 			//	Receive piece values dictionary
             var pieceValues = resp.responseJSON
 			
-			//	Update values text object
-            valueText.text("Values:\t" + JSON.stringify(pieceValues).replace(/\"/g,'').replace(/\{/g,'').replace(/\}/g,'').replace(/\,/g,', '))
+			//	Round values for display in valueText object
+			var roundedValues = pieceValues
+			for (i in roundedValues)
+				roundedValues[i] = roundedValues[i].toFixed(2)
+			valueText.text("Values:\t" + JSON.stringify(roundedValues).replace(/\"/g,'').replace(/\{/g,'').replace(/\}/g,'').replace(/\,/g,', '))
 			
 			//	Loop over piece values and draw colors in appropriate squares
             for (var s in pieceValues) {
@@ -98,6 +123,57 @@ $( document ).ready(function() {
 		if (source != null && source != 'spare') {
 			clearSquare(source)
 		}
+	}
+	
+	//	Function to run on drop of piece (before onSnapEnd)
+	function onDrop(source, target, piece, newPos, oldPos, orientation){
+		
+		//	If moved off board, remove from chessjs object, clear square color, return 'trash'
+		if (target == 'offboard') {
+			game.remove(source)
+			clearSquare(source)
+			messageText.text('Message: Piece manually removed, circumventing rules')
+			return 'trash'
+		}
+		
+		//	If piece is already there, snap back
+		if (board.position()[target] == piece) {
+			return 'snapback'
+		}
+		
+		//	If piece coming from spares, add it to chessjs game
+		if (source == 'spare') {
+			tryPut = game.put({ type: piece[1].toLowerCase(), color: piece[0] }, target)
+			
+			//	If not allowed to put on chessjs board, snap back
+			if (!tryPut) {
+				return 'snapback'
+			}
+			messageText.text('Message: Piece manually added, circumventing rules')
+		}
+		
+		//	If moving piece on the board, try making move in chessjs object
+		else {
+			//	Try moving piece, assuming queen promotion if necessary
+			tryMove = game.move({to:target , from:source, promotion:'q'})
+			
+			//	If move not allowed, snap back
+			if (!tryMove) {
+				messageText.text('Message: Move not legal')
+				return 'snapback'
+			}
+			messageText.text('Message: Legal move made')
+		}
+	}
+	
+	function onChange(){
+		var nextMove = game.turn()
+		var nextMoveText
+		if (nextMove == 'b')
+			nextMoveText = 'Black'
+		else
+			nextMoveText = 'White'
+		nextTurnText.text('Next Turn: ' + nextMoveText)
 	}
 	
 	// Run Function
