186660fe72e9e7beb170f5f7ac63cfdb29b08eb1
mtulpule <56699366+mtulpule@users.noreply.github.com>
2019-11-18 17:35:56 -0500

    Added bar charts back in

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -1,20 +1,18 @@
 $( document ).ready(function() {
-    
+
 	//	Board name that matches div
     var boardName = 'myBoard'
-	
+
 	//	Initialize chess js object
 	var game = new Chess()
-	
+
 	//	Start chess as cleared
 	game.clear()
-	
+
+
     //  Hard code start configuration
 	var positions = {}
-    
-    // variable for square for highlights
-	var squareClass = 'square-55d63'
-	
+
     //  Initialize board configuration
 	var boardConfig = {
 		pieceTheme: '/static/{piece}.png',		//	location of piece images
@@ -26,7 +24,8 @@ $( document ).ready(function() {
 		onMouseoverSquare:onMouseoverSquare,	//	Function to call when mouse is hovering over square
 		sparePieces: true						//	Draws spare black and white pieces on either side of the board
 	}
-    
+
+
     //  SVG for text below chess board
 	var svgW = 400
 	var svgH = 100
@@ -35,45 +34,46 @@ $( document ).ready(function() {
 	var svg = d3.select('#boardConfig')
 		.append('svg')
 		.attr("width", svgW)
-		.attr("height", svgH)		
-	
+		.attr("height", svgH)
+
 	//	Text object for listing positions
 	var positionText = svg.append('text')
 		.attr('x',0)
 		.attr('y', 0)
 		.attr('dominant-baseline', 'hanging')
-		.attr('text-anchor', 'start')  
+		.attr('text-anchor', 'start')
 		.style('font-size', svgTextSize + 'px')
-    
+
 	//	Text object for listing values
     var valueText = svg.append('text')
 		.attr('x',0)
 		.attr('y', svgVerticalSpace)
 		.attr('dominant-baseline', 'hanging')
-		.attr('text-anchor', 'start')  
+		.attr('text-anchor', 'start')
 		.style('font-size', svgTextSize + 'px')
-		
+
 	//	Text that indicates next move
 	var nextTurnText = svg.append('text')
 		.attr('x',0)
 		.attr('y', 2*svgVerticalSpace)
 		.attr('dominant-baseline', 'hanging')
-		.attr('text-anchor', 'start')  
+		.attr('text-anchor', 'start')
 		.style('font-size', svgTextSize + 'px')
 		.text('Next Turn: ')
-		
+
 	//	Text that contains message to user
 	var messageText = svg.append('text')
 		.attr('x',0)
 		.attr('y', 3*svgVerticalSpace)
 		.attr('dominant-baseline', 'hanging')
-		.attr('text-anchor', 'start')  
+		.attr('text-anchor', 'start')
 		.style('font-size', svgTextSize + 'px')
 		.text('Message: ')
-    
+
     //  Create chess board
 	var board = Chessboard(boardName,boardConfig)
-    
+
+
 	//	Colorbar
     var colorBarWidth = 400
     var colorBarHeight = 20
@@ -91,61 +91,116 @@ $( document ).ready(function() {
         .attr('height',20)
         .style('fill',color(b/numBars))
     }
-	
-	//removes square highlights
-	//function removeHighlights
-	//{
-  	//	board.find('.' + squareClass)
-    //		.removeClass('highlight')
-	//}
-	
+
 	//	Clears custom color under a single square
 	function clearSquare(square){
 		$('#myBoard .square-' + square).css('background', '')
 	}
 
     //  Function ran when piece moves
-	function onSnapEnd(source=null, target=null, piece=null){	
-		
-		//highlight move
-		//removeHighlights()
-		//board.find('.square-' + source).addClass('highlight')
-		//board.find('.square-' + target).addClass('highlight-white')
-		
+	function onSnapEnd(source=null, target=null, piece=null){
+
 		//	Get current board positions
 		var boardPositions = board.position()
-		
+
 		//	Update positions text object
 		positionText.text("Positions:\t" + JSON.stringify(boardPositions).replace(/\"/g,'').replace(/\{/g,'').replace(/\}/g,'').replace(/\,/g,', '))
-		
+
 		//	POST to python and receive values
 		$.post( "/postmethod", boardPositions, function(err, req, resp){
-			
+
 			//	Receive piece values dictionary
             var pieceValues = resp.responseJSON
-			
+            console.log(pieceValues)
 			//	Round values for display in valueText object
 			var roundedValues = pieceValues
 			for (i in roundedValues)
 				roundedValues[i] = roundedValues[i].toFixed(2)
 			valueText.text("Values:\t" + JSON.stringify(roundedValues).replace(/\"/g,'').replace(/\{/g,'').replace(/\}/g,'').replace(/\,/g,', '))
-			
+
 			//	Loop over piece values and draw colors in appropriate squares
             for (var s in pieceValues) {
 				$('#myBoard .square-' + s).css('background', color(pieceValues[s]))
             }
+      $('#whiteChart').remove();
+      $('#content').append('<canvas id="whiteChart"></canvas>')
+
+      $('#blackChart').remove();
+      $('#content').append('<canvas id="blackChart"></canvas>')
+      var ctW = document.getElementById('whiteChart').getContext('2d');
+      console.log(board.position())
+
+      pieces = boardPositions;
+      whitePieces = [];
+      whiteValues = [];
+      for (var key in pieces) {
+        if (pieces[key].charAt(0) == 'w') {
+          whitePieces.push(pieces[key]);
+          whiteValues.push(pieceValues[key]);
+        }
+      }
+      var whiteChart = new Chart(ctW, {
+          // The type of chart we want to create
+          type: 'bar',
+
+          // The data for our dataset
+          data: {
+              labels: whitePieces,
+              datasets: [{
+                  label: 'White Piece Values',
+                  backgroundColor: 'rgb(255, 99, 132)',
+                  borderColor: 'rgb(255, 99, 132)',
+                  data: whiteValues
+              }]
+          },
+
+          // Configuration options go here
+          options: {}
+      });
+
+      var ctB = document.getElementById('blackChart').getContext('2d');
+      console.log(board.position())
+
+      pieces = boardPositions;
+      blackPieces = [];
+      blackValues = [];
+      for (var key in pieces) {
+        if (pieces[key].charAt(0) == 'b') {
+          blackPieces.push(pieces[key]);
+          blackValues.push(pieceValues[key]);
+        }
+      }
+      var blackChart = new Chart(ctB, {
+          // The type of chart we want to create
+          type: 'bar',
+
+          // The data for our dataset
+          data: {
+              labels: blackPieces,
+              datasets: [{
+                  label: 'Black Piece Values',
+                  backgroundColor: 'rgb(0, 0, 0)',
+                  borderColor: 'rgb(0, 0, 0)',
+                  data: blackValues
+              }]
+          },
+
+          // Configuration options go here
+          options: {}
+      });
 		});
-		
+
 		//	If piece isn't added from spare pieces and source isn't null, clear source square
 		//	source is only null in manual run of function below, to initialize the values
 		if (source != null && source != 'spare') {
 			clearSquare(source)
 		}
+
 	}
-	
+
 	//	Function to run on drop of piece (before onSnapEnd)
 	function onDrop(source, target, piece, newPos, oldPos, orientation){
-		
+
 		//	If moved off board, remove from chessjs object, clear square color, return 'trash'
 		if (target == 'offboard') {
 			game.remove(source)
@@ -153,28 +208,28 @@ $( document ).ready(function() {
 			messageText.text('Message: Piece manually removed, circumventing rules')
 			return 'trash'
 		}
-		
+
 		//	If piece is already there, snap back
 		if (board.position()[target] == piece) {
 			return 'snapback'
 		}
-		
+
 		//	If piece coming from spares, add it to chessjs game
 		if (source == 'spare') {
 			tryPut = game.put({ type: piece[1].toLowerCase(), color: piece[0] }, target)
-			
+
 			//	If not allowed to put on chessjs board, snap back
 			if (!tryPut) {
 				return 'snapback'
 			}
 			messageText.text('Message: Piece manually added, circumventing rules')
 		}
-		
+
 		//	If moving piece on the board, try making move in chessjs object
 		else {
 			//	Try moving piece, assuming queen promotion if necessary
 			tryMove = game.move({to:target , from:source, promotion:'q'})
-			
+
 			//	If move not allowed, snap back
 			if (!tryMove) {
 				messageText.text('Message: Move not legal')
@@ -183,7 +238,7 @@ $( document ).ready(function() {
 			messageText.text('Message: Legal move made')
 		}
 	}
-	
+
 	//	When board positions change, update whose move is next
 	function onChange(){
 		var nextMove = game.turn()
@@ -194,7 +249,7 @@ $( document ).ready(function() {
 			nextMoveText = 'White'
 		nextTurnText.text('Next Turn: ' + nextMoveText)
 	}
-	
+
 	//	Function that runs when clear board button is clicked
 	$('#clearBoardBtn').on('click', function(){
 		oldPositions =  board.position()
@@ -207,9 +262,67 @@ $( document ).ready(function() {
 		valueText.text("Values:\t")
 		messageText.text("Message: Board and game cleared.")
 		onChange()
-		
+
+    // making a dummy chart with no data whenever board is cleared
+    $('#whiteChart').remove();
+    $('#content').append('<canvas id="whiteChart"></canvas>')
+
+    $('#blackChart').remove();
+    $('#content').append('<canvas id="blackChart"></canvas>')
+
+    var ctW = document.getElementById('whiteChart').getContext('2d');
+    console.log(board.position())
+
+
+    whitePieces = [];
+    whiteValues = [];
+
+    var whiteChart = new Chart(ctW, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: whitePieces,
+            datasets: [{
+                label: 'White Piece Values',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: whiteValues
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+
+    var ctB = document.getElementById('blackChart').getContext('2d');
+    console.log(board.position())
+
+    blackPieces = [];
+    blackValues = [];
+
+    var blackChart = new Chart(ctB, {
+        // The type of chart we want to create
+        type: 'bar',
+
+        // The data for our dataset
+        data: {
+            labels: blackPieces,
+            datasets: [{
+                label: 'Black Piece Values',
+                backgroundColor: 'rgb(0, 0, 0)',
+                borderColor: 'rgb(0, 0, 0)',
+                data: blackValues
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+
 	})
-	
+
 	//	Function that runs when start position button is clicked
 	$('#startPositionBtn').on('click', function(){
 		board.start()
@@ -217,17 +330,19 @@ $( document ).ready(function() {
 		onSnapEnd()
 		messageText.text("Message: Board and game reset to start")
 	})
-	
+
 	//	Function called when mouseover square
 	//	TODO: implement bar chart highlight
 	function onMouseoverSquare(square, piece){
 		if (piece != false)
 			console.log('Mouse hovered over ' + piece + ' at ' +  square)
-		else 
+		else
 			console.log('Mouse hovered over ' + square)
 	}
-	
+
 	// Run Function
     onSnapEnd()
-  
+
+
+
 });
