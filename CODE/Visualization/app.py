9aa549cf50883866ba35eb84f5bb8762ee3fffde
Travis Burrows <37048572+tjburrows@users.noreply.github.com>
2019-12-05 14:29:02 -0500

    added comments to app.py

--- CODE/Visualization/app.py
+++ CODE/Visualization/app.py
@@ -17,16 +17,19 @@ elif numArguments == 2:
 elif numArguments == 1:
 	port = 5001
 	
+# Initialize Flask app
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
 app.debug = True
 app._static_folder = path.abspath("templates/static/")
-						   
+				
+# Define GET route				
 @app.route('/', methods=['GET'])
 def chess():
     title = 'CSE6242 Team 44 Chess App'
     return render_template('layouts/chess.html', title=title)
 
+# Define POST route	
 @app.route('/postmethod', methods = ['POST'])
 def post_javascript_data():
     board_positions = request.form.to_dict()
